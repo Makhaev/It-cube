13e2fa020fcd1dd1ea87231a52fd1260e706a59c
Adam <maxaev.adam@bk.ru>
2025-04-30 11:04:39 +0300

    первый коммит

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import logo from './img/logo.jpg';
+import background from './img/background.jpg'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='header'>
+        <div className='logo'>
+          <img className='img-logo' src={logo}/>
+        </div>
+        <nav className='header-nav'>
+          <ul className='menu'>
+            <li className='menu-list'>Новости</li>
+            <li className='menu-list'>Конкурсы</li>
+            <li className='menu-list'>Документы</li>
+            <li className='menu-list'>Федеральный проект</li>
+          </ul>
+        </nav>
+
       </header>
     </div>
   );
